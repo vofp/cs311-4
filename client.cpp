0a56cd87c45a7249a76539346f3a769ca00ec053
Francis Vo <firefreakfrancis@gmail.com>
2012-03-10 08:49:10 -0800

    find perfects done

--- client.cpp
+++ client.cpp
@@ -1,20 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
-
+#include <math.h>
 
 long factor_pair(long number, long factor);
 bool is_perfect(long number);
+int sum_factors(long number);
 
 int main(int argc, char const *argv[]){
 	long test = strtol( argv[1], NULL, 10 );
-	long pair = factor_pair(test, 2);
-
-	std::cout << "one factor pair of " << test << " is 2 and " << pair << std::endl;
+	for(int i = 2; i <= test ; ++i){
+		is_perfect(i);
+	}
+	
 }
 
 bool is_perfect(long number){
-	return false;
+	return sum_factors(number) == number;
+}
+
+int sum_factors(long number){
+	long sum = 0;
+	for(long factor = 2; factor <= sqrt(number); ++factor){
+		long pair;
+		if((pair = factor_pair(number, factor)) != 0){
+			if(number != factor){
+				sum = sum + factor + pair;
+				if(factor == pair){
+					sum -= factor;
+				}
+			}
+		}
+	}
+	return sum + 1;
 }
 
 long factor_pair(long number, long factor){
