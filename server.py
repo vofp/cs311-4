e7390fee5807203a1fe6f945092021f4f58e5555
Francis Vo <firefreakfrancis@gmail.com>
2012-03-11 00:13:34 -0800

    parse client message

--- server.py
+++ server.py
@@ -8,9 +8,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port)) 
 s.listen(backlog) 
 while 1: 
-    client, address = s.accept() 
-    data = client.recv(size) 
-    if data: 
-        client.send(data+" - from server") 
-    client.close()
+	client, address = s.accept() 
+	data = client.recv(size) 
+	if data:
+		split(data)[0]
+		client.send("From server: " + split(data)[0]) 
+	client.close()
 
