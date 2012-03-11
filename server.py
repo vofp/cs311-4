ab5a6e342e80959a94f20a6d28eac2fc33bf45e6
Francis Vo <firefreakfrancis@gmail.com>
2012-03-11 14:39:00 -0700

    Updated server to parse different messages

--- server.py
+++ server.py
@@ -13,13 +13,25 @@ while 1:
 	data = client.recv(size) 
 	if data:
 		data_array = data.split(" ")
-		if data_array[0] == 'IOPS':
+		if data_array[0] == 'REQ':
 			iops = float(data_array[1])/1000000.0
 			divisions = 15/iops*1000000000
 			lower = check
 			while(divisions > 0):
 				divisions -= check
 				check += 1
-			client.send('From server: '+str(lower) + ' '+ str(check-1)) 
+			client.send('RES '+str(lower) + ' '+ str(check-1))
+		elif data_array[0] == 'PFN':
+			print 'Perfect: ' + str(data_array[1])
+			client.send('ACK PFN')
+		elif data_array[0] == 'DON':
+			print 'Range Done: ' + str(data_array[1]) + ' to ' + str(data_array[2])
+			client.send('ACK DON')
+		elif data_array[0] == 'EXT':
+			print 'Exit requested'
+			client.send('ACK EXT')
+		else:
+			print 'Unknown data: 'data
+			client.send('NAK')
 	client.close()
 
