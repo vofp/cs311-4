fb5bcebcffa8d6850715325c9c58090cdccb5c1d
Francis Vo <firefreakfrancis@gmail.com>
2012-03-12 09:54:38 -0700

    server exit from signal

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-import socket 
+import socket
+import sys 
 
 host = '' 
 port = 7331
@@ -8,30 +9,38 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port)) 
 s.listen(backlog)
 check = 2
-client, address = s.accept()
 while 1:
-	data = client.recv(size) 
-	if data:
-		data_array = data.split(" ")
-		if data_array[0] == 'REQ':
-			iops = float(data_array[1])/1000000.0
-			divisions = 15/iops*1000000000
-			lower = check
-			while(divisions > 0):
-				divisions -= check
-				check += 1
-			client.send('RES '+str(lower) + ' '+ str(check-1))
-			print 'request from ' + str(lower) + ' to ' + str(check-1)
-		elif data_array[0] == 'PFN':
-			print 'Perfect: ' + str(data_array[1])
-			client.send('ACK PFN')
-		elif data_array[0] == 'DON':
-			print 'Range Done: ' + str(data_array[1]) + ' to ' + str(data_array[2])
-			client.send('ACK DON')
-		elif data_array[0] == 'EXT':
-			print 'Exit requested'
-			client.send('ACK EXT')
-		else:
-			print 'Unknown data: ' + data
-			client.send('NAK')
-#client.close()
+	client, address = s.accept()
+	print 'Connected by', address
+	while 1:
+		data = client.recv(size) 
+		if data:
+			data_array = data.split(" ")
+			if data_array[0] == 'REQ':
+				iops = float(data_array[1])/1000000.0
+				divisions = 15/iops*1000000000
+				lower = check
+				while(divisions > 0):
+					divisions -= check
+					check += 1
+				client.send('RES '+str(lower) + ' '+ str(check-1))
+				print 'request from ' + str(lower) + ' to ' + str(check-1)
+			elif data_array[0] == 'PFN':
+				print 'Perfect: ' + str(data_array[1])
+				client.send('ACK PFN')
+			elif data_array[0] == 'DON':
+				print 'Range Done: ' + str(data_array[1]) + ' to ' + str(data_array[2])
+				client.send('ACK DON')
+			elif data_array[0] == 'CLS':
+				print address, ' Closing'
+				client.send('ACK CLS')
+				client.close()
+				break
+			elif data_array[0] == 'EXT':
+				print 'Exit requested'
+				client.send('ACK EXT')
+				sys.exit()
+			else:
+				print 'Unknown data: ' + data
+				client.send('NAK')
+	client.close()
