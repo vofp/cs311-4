94568c327ee77ad2f9bf8b2d1427bcf085caa99f
Francis Vo <firefreakfrancis@gmail.com>
2012-03-14 09:05:19 -0700

    only displays perfects

--- report.py
+++ report.py
@@ -10,8 +10,10 @@ client_socket.connect((host,port))
 client_socket.send('RPT 0') 
 data = client_socket.recv(size)
 index = 1
+print "Perfects:"
 while(data != 'NIL'):
-	print 'Received:', data
+	data_array = data.split(" ")
+	print data_array[1]
 	client_socket.send('RPT '+ str(index))
 	data = client_socket.recv(size) 
 	index += 1
